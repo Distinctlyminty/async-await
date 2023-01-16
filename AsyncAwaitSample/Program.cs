f97a96b13c7368eb287468b56f704dfe61f9feac
James MIllar <jmillar@sailr.co.uk>
2023-01-16 17:53:06 +0000

    Initial commit

---
+++ AsyncAwaitSample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AsyncAwaitSample;
+class Program
+{
+    public static async Task Main()
+    {
+        // the await keyword ensures execution waits for this method call to complete
+        await DelayAsync();
+        Console.WriteLine("Program complete.");
+    }
+
+    public async static Task DelayAsync()
+    {
+        await Task.Delay(2000);
+        Console.WriteLine("Delay complete.");
+    }
+}
+
